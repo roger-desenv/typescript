f617a36effdf271de2cf70189318e4459d714423
Roger Costa <roger.desenv@gmail.com>
2022-10-03 18:00:32 -0300

    adicionando arquivo

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//Para tipagem de constantes, basta inserir o tipo ao lado da contante.
+let x = 10;
+x = 21;
+//inferencia x annotation
+let y = 10; // inferencia
+let z = 20; //annotation
+// tipos básicos
+let firstName = "Roger";
+let age = 35;
+const isAdmin = true;
+//object ou arrays
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
+console.log(myNumbers.length);
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+//Para tipagem de constantes, basta inserir o tipo ao lado da contante.
+let x: number = 10 
+
+x = 21
+
+
+//inferencia x annotation
+let y = 10 // inferencia
+let z: number = 20 //annotation
+
+
+// tipos básicos
+let firstName: string = "Roger"
+let age: number = 35
+const isAdmin: boolean = true
+
+
+//object ou arrays
+const myNumbers: number[] = [1, 2, 3]
+
+console.log(myNumbers)
+console.log(myNumbers.length)
+
