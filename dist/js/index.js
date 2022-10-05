eeba7c4003f2a086046e2fc6ad61ae015f4ce09d
Roger Costa <roger.desenv@gmail.com>
2022-10-05 10:51:07 -0300

    adicionando tipos

--- dist/js/index.js
+++ dist/js/index.js
@@ -13,3 +13,96 @@ const isAdmin = true;
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
+//tuple
+let myTuple;
+myTuple = [5, "teste", ["a", "b"]];
+//object literals => {prop: value}
+const user = {
+    name: "Pedro",
+    age: 18
+};
+console.log(user);
+//any: pode ser usada para qualquer tipo de dado
+let a = 0;
+a = "teste";
+a = true;
+a = [];
+//union type, você pode definir mais de um tipo para variável.
+let id = 200;
+id = "João";
+id = true;
+const userId = 10;
+const productId = "00001";
+const shirId = 123;
+//enum, utilizado para evitar repetições de códigos.
+var Size;
+(function (Size) {
+    Size["P"] = "Pequeno";
+    Size["M"] = "M\u00E9dio";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M
+};
+console.log(camisa);
+//literal types, não é permitido alterar o valor predefinido.
+let teste;
+teste = "algumvalor";
+teste = null;
+//funcoes
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+function sayHelloTo(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHelloTo("Roger"));
+function logger(msg) {
+    console.log(msg);
+}
+logger("Teste!");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+const someNumbers = {
+    n1: 5,
+    n2: 10
+};
+console.log(multiplyNumbers(someNumbers));
+//narrowing, checagem de tipos
+function doSomething(info) {
+    if (typeof info === "number") {
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número"); // Neste caso o ELSE está implícito
+}
+//doSomething é o typeOf do Typescript
+doSomething(5);
+doSomething(true);
+//generics
+function showArrayItems(arr) {
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+showArrayItems(a1);
+showArrayItems(a2);
+//classes
+class User {
+    constructor(name, role, isApproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+}
+const zeca = new User("Zéca", "Admin", true);
+console.log(zeca);
--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,143 @@ const myNumbers: number[] = [1, 2, 3]
 console.log(myNumbers)
 console.log(myNumbers.length)
 
+//tuple
+let myTuple: [number, string, string[]]
+
+myTuple = [5, "teste", ["a", "b"]]
+
+//object literals => {prop: value}
+const user: {name: string, age: number} = {
+    name: "Pedro",
+    age: 18
+}
+
+console.log(user)
+
+
+//any: pode ser usada para qualquer tipo de dado
+
+let a:any = 0
+
+a = "teste"
+a = true
+a = []
+
+//union type, você pode definir mais de um tipo para variável.
+let id: string | number | boolean = 200
+id = "João"
+id = true
+
+//type alias
+type myIdType = number | string
+const userId: myIdType = 10
+const productId: myIdType = "00001"
+const shirId: myIdType = 123
+
+
+//enum, utilizado para evitar repetições de códigos.
+enum Size {
+    P = "Pequeno",
+    M = "Médio",
+    G = "Grande"
+}
+
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M
+}
+console.log(camisa)
+
+
+//literal types, não é permitido alterar o valor predefinido.
+let teste: "algumvalor" | null
+teste = "algumvalor"
+teste = null
+
+
+//funcoes
+function sum(a: number, b:number) {
+    return a + b
+}
+
+console.log(sum(12,12))
+
+function sayHelloTo(name: string): string {
+    return `Hello ${name}`
+}
+console.log(sayHelloTo("Roger"))
+
+function logger(msg: string): void { //void utilizado em funções que não tem valor prédefinido.
+    console.log(msg)
+}
+
+logger("Teste!")
+
+
+//interface
+interface MathFunctionParams {
+    n1: number,
+    n2: number
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2
+}
+
+console.log(sumNumbers({ n1: 1, n2: 2 }))
+
+
+function multiplyNumbers(nums: MathFunctionParams) {
+    return nums.n1 * nums.n2
+}
+
+const someNumbers: MathFunctionParams= {
+    n1:5,
+    n2:10
+}
+
+console.log(multiplyNumbers(someNumbers))
+
+
+//narrowing, checagem de tipos
+function doSomething(info: number | boolean) {
+    if(typeof info === "number") {
+        console.log(`O número é ${info}`)
+        return
+    }
+    console.log("Não foi passado um número") // Neste caso o ELSE está implícito
+}
+
+//doSomething é o typeOf do Typescript
+doSomething(5)
+doSomething(true)
+
+
+//generics
+function showArrayItems<T>(arr: T[]) { // Os mais usados são <T> e <U>
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`)
+    })
+}
+
+const a1 = [1,2,3]
+const a2 = ["a", "b","c"]
+
+showArrayItems(a1)
+showArrayItems(a2)
+
+//classes
+class User {
+    name
+    role
+    isApproved
+
+    constructor(name: string, role:string, isApproved: boolean) {
+        this.name = name
+        this.role = role
+        this.isApproved = isApproved
+    }
+}
+
+const zeca = new User("Zéca", "Admin", true)
+console.log(zeca)
